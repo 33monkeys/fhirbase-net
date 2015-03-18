4d74d7f3bbf217e26b8ea4e6ed6cec48faa1e9d8
a.lisnyi <a.lisnyi@netrika.ru>
2015-03-18 15:49:26 +0300

    Partially FhirbaseApi and tests

---
+++ src/Fhirbase.Net/Fhirbase.Net.Test/ApiTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fhirbase.Net.Test
+{
+    [TestFixture]
+    public class ApiTest
+    {
+        [Test]
+        public void Test_Create()
+        {
+        }
+    }
+}
